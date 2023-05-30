536fe5a0bc5733e879247afd4e3ff38d3d78ce1f
ZHKe <kezhanghan@outlook.com>
2023-05-30 11:38:50 -0700

    Add v1.1.0 information

--- torchflow/version.py
+++ torchflow/version.py
@@ -1,12 +1,13 @@
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 __name__ = 'torchflow'
 __description__ = 'A PyTorch-Based Framework'
-__url__ = ''
-__license__ = 'https://github.com/ZHKKKe/torchflow'
+__url__ = 'https://github.com/ZHKKKe/torchflow'
+__license__ = ''
 __author__ = 'Zhanghan Ke'
 __author_email__ = 'kezhanghan@outlook.com'
 
 __updates__ = {
     '1.0.0': 'Initial version',
+    '1.1.0': 'Support (1) torch distributed training, (2) flow independent optimizers, and (3) run a flow at a certain interval'
 }
